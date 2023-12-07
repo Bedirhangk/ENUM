15f43b4004fb664eae0cc494fd20f7b8e69ce517
Bedirhan Gokkaya <bedirhangk12@gmail.com>
2023-12-07 19:52:26 +0300

    Enum Konusu Çalışıldı.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ENUM;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Console.WriteLine(Gunler.Pazar);
+       Console.WriteLine((int)Gunler.Pazar); 
+
+       Console.WriteLine();
+
+       int Sıcaklık=12;
+       if(Sıcaklık<=(int)HavaDurumu.Normal && Sıcaklık>(int)HavaDurumu.Soğuk)
+        Console.WriteLine("Hava Biraz Soğuk. Üstğne Hırka Alabilirsin.");
+       else if(Sıcaklık>=(int)HavaDurumu.Sıcak)
+        Console.WriteLine("Hava Çok Sıcak. Denize Atla.");
+       else
+        Console.WriteLine("Hava Çok Soğuk!!!!");
+        
+    }
+}
+
+enum Gunler 
+{
+    Pazartesi=1, // default olarak ilk yazılan 0 index numarasına sahip olur. Bunu =1 yaparak başlangıcı değiştirebilirsiniz.
+    Salı,
+    Çarşamba,
+    Perşembe,
+    Cuma,
+    Cumartesi,
+    Pazar
+}
+
+enum HavaDurumu
+{
+    Soğuk=5,
+    Normal=20,
+    Sıcak=35
+}
